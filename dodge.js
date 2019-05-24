52d52f7a2035d0e4175fa34d4648e162aa0acfca
noashi <mrerfrg@gmail.com>
2019-05-25 03:06:41 +0900

    [update]円の動きをランダムに
    円が壁にぶつかった際、次に動く方向をランダムで取得するようにしました。

--- dodge.js
+++ dodge.js
@@ -19,6 +19,36 @@ function drawArc() {
   ctx.fill();
   ctx.closePath();
 }
+
+//円の色を決める関数
+function coloringArc() {
+  //カラーコードを生成・円の色を決める変数へ代入
+  var randomColor = "rgb(" + (~~(256 * Math.random())) + ", " + (~~(256 * Math.random())) + ", " + (~~(256 * Math.random())) + ")";
+  arcColor = randomColor;
+  return arcColor;
+}
+
+//円の動く方向をランダムに指定する関数
+function randomDirection() {
+  //円の動く方向・スピードをランダムに指定（xとyの増減数を、1から10の間でランダムに取得）
+  randomDirectionX = Math.ceil(Math.random() * 10);
+  randomDirectionY = Math.ceil(Math.random() * 10);
+  if (dx < 0) {
+    dx = randomDirectionX;
+  } else {
+    dx = -randomDirectionX;
+  }
+  if (dy < 0) {
+    dy = randomDirectionY;
+  } else {
+    dy = -randomDirectionY;
+  }
+  return {
+    dx,
+    dy
+  };
+}
+
 //描画処理
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -27,12 +57,13 @@ function draw() {
   //円が壁に到達したときの処理
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     //横軸に対してボールを跳ね返す
-    dx = -dx;
+    randomDirection();
+    coloringArc();
   }
-  //円が壁に到達したときの処理
   if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
     //縦軸に対してボールを跳ね返す
-    dy = -dy;
+    randomDirection();
+    coloringArc();
   }
   //ボールを動かす
   x += dx;
